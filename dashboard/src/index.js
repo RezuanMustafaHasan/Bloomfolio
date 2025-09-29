9d7b8aaa48d5b1ac5ae074cce5fba78585e6aecc
Rezuan <hasanideal2002@gmail.com>
2025-09-29 20:02:14 +0600

    AuthN & AuthZ in frontend

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CookiesProvider } from 'react-cookie';
 import "./index.css";
-import Home from "./components/Home";
-
+import 'react-toastify/dist/ReactToastify.css';
+import Home from './components/Home'
+// import { Login, Signup } from "./pages";
+// import Home from "./pages/Home";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <CookiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='*' element={<Home/>}/>
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </CookiesProvider>
   </React.StrictMode>
 );
 
